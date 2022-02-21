9009af0c4ff4a119825c50982357831f10dda25d
aparna21A91A6137 <97721064+aparna21A91A6137@users.noreply.github.com>
2022-02-21 13:56:56 +0530

    #include<stdio.h> int main() {     int l,b,w,c,area;     scanf("%d %d %d %d",&l,&b,&w,&c);     if(l<=2*w||b<=2*w)     {         printf("Impossible");     }     else     {         area=l*b-((l-2*w)*(b-2*w));         printf("%d",area*c);     }     return 0; }

---
+++ Cost_to_fence_the_park_inside.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    int l,b,w,c,area;
+    scanf("%d %d %d %d",&l,&b,&w,&c);
+    if(l<=2*w||b<=2*w)
+    {
+        printf("Impossible");
+    }
+    else
+    {
+        area=l*b-((l-2*w)*(b-2*w));
+        printf("%d",area*c);
+    }
+    return 0;
+}
+
