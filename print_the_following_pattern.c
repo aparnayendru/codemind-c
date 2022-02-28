9228262169ebaef475f116dfc721f7e58fd4d144
aparna21A91A6137 <97721064+aparna21A91A6137@users.noreply.github.com>
2022-02-28 15:43:16 +0530

    #include<stdio.h> int main() {     int i,j,n,k;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(k=1;k<=(n-i);k++)         {             printf(" ");         }         for(j=1;j<=(2*i-1);j++)         {             printf("%d",i);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int i,j,n,k;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=n;j>0;j--)
+        for(k=1;k<=(n-i);k++)
         {
-            printf("%d ",j);
+            printf(" ");
+        }
+        for(j=1;j<=(2*i-1);j++)
+        {
+            printf("%d",i);
         }
         printf("
 ");
