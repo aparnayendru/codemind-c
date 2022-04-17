08d3aab858add3582bb82b1c2380fef56a8afe20
aparna21A91A6137 <97721064+aparna21A91A6137@users.noreply.github.com>
2022-04-17 20:33:07 +0530

    #include<stdio.h> int main() {     char s[1000];     int sum=0,i;     scanf("%s",s);     for(i=0;s[i]!=NULL;i++)     {         if(s[i]>='0'&&s[i]<='9')         sum+=(s[i]-'0');     }     printf("%d",sum);     return 0; }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char s[1000];
+    int sum=0,i;
+    scanf("%s",s);
+    for(i=0;s[i]!=NULL;i++)
+    {
+        if(s[i]>='0'&&s[i]<='9')
+        sum+=(s[i]-'0');
+    }
+    printf("%d",sum);
+    return 0;
+}
