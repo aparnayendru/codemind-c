290bd054da68dc4130e1e9fe55eb8c6381cbe6e7
aparna21A91A6137 <97721064+aparna21A91A6137@users.noreply.github.com>
2022-04-23 19:15:47 +0530

    #include<stdio.h> void fibonacci(int,int,int); int main() {     int n1=0,n2=1,n3,n,i;     scanf("%d",&n);     printf("%d %d ",n1,n2);     fibonacci(n1,n2,n);     return 0;
    }
    void fibonacci(int n1,int n2,int n)
    {
        int i,n3;
        for(i=3;i<=n;i++)
        {
            n3=n1+n2;
            printf("%d ",n3);
            n1=n2;
            n2=n3;
        }
    }

--- fibonacci.c
+++ fibonacci.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
+void fibonacci(int,int,int);
 int main()
 {
-    int n,i,n1=0,n2=1,n3;
+    int n1=0,n2=1,n3,n,i;
     scanf("%d",&n);
     printf("%d %d ",n1,n2);
-    for(i=2;i<n;i++)
+    fibonacci(n1,n2,n);
+    return 0;
+    
+}
+void fibonacci(int n1,int n2,int n)
+{
+    int i,n3;
+    for(i=3;i<=n;i++)
     {
         n3=n1+n2;
         printf("%d ",n3);
         n1=n2;
         n2=n3;
     }
-    return 0;
 }
