1a67bfe4dc2ac35330eedfd6ae15f8ecb1090b46
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-06-07 10:53:53 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100];     int i,len;     scanf("%[^ ]",str);     len=strlen(str);     for(i=len-1;i>=0;i--)     {         printf("%c",str[i]);     }     return 0; }

--- Reverse_String.c
+++ Reverse_String.c
@@ -3,30 +3,13 @@
 int main()
 {
     char str[100];
-    int i,j,end,start,len;
+    int i,len;
     scanf("%[^
 ]",str);
     len=strlen(str);
-    end=len-1;
     for(i=len-1;i>=0;i--)
     {
-        if(str[i]==' '||i==0)
-        {
-            if(i==0)
-            {
-                start=0;
-            }
-            else
-            {
-                start=i+1;
-            }
-            for(j=start;j<=end;j++)
-            {
-                printf("%c",str[j]);
-            }
-            end=i-1;
-            printf(" ");
-        }
+        printf("%c",str[i]);
     }
     return 0;
 }
