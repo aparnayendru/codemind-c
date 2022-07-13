22eb4c43857b93e4616602d575d80e03c815f430
aparna yendru <97721064+aparna21A91A6137@users.noreply.github.com>
2022-07-13 10:16:54 +0530

    #include<stdio.h> int main() {     int n,i,countt=0,sum=0,k,count=0,j,m;     scanf("%d ",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&m);     for(i=0;i<n;i++)     {         count=0;         k=arr[i];         for(j=1;j<=k;j++)         {             if(k%j==0)             {                 count++;             }         }         if(count==2)         {             countt++;         }     }     printf("%d",countt); }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,countt=0,sum=0,k,count=0,j,m;
+    scanf("%d ",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+    {
+        count=0;
+        k=arr[i];
+        for(j=1;j<=k;j++)
+        {
+            if(k%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            countt++;
+        }
+    }
+    printf("%d",countt);
+}
